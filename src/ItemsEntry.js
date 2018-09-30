9b3900989d83acbd39968ca857119c2ca14d3fde
Alex Alexandrescu <alexandrescu.alex@gmail.com>
2018-09-30 22:23:01 +0300

    Better UX for item entry with InputGroups rather than Select and Textarea

--- src/ItemsEntry.js
+++ src/ItemsEntry.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
-import { Input, Button, Row } from "reactstrap";
 import { connect } from "react-redux";
+import {
+  Button,
+  Input,
+  InputGroup,
+  Row,
+  InputGroupButtonDropdown,
+  DropdownMenu,
+  DropdownItem,
+  DropdownToggle
+} from "reactstrap";
 import { addItems, selectCollection } from "./actions";
 
 class ItemsEntry extends Component {
@@ -9,50 +18,83 @@ class ItemsEntry extends Component {
 
     this.state = {
       textareaVal: "",
-      currentCollection: this.props.currentCollection
+      currentCollection: this.getCollectionName(this.props.currentCollection),
+      dropdownOpen: false,
+      splitButtonOpen: false
     };
 
     this.onClickAddValues = this.onClickAddValues.bind(this);
-    this.onCurrentCollectionChange = this.onCurrentCollectionChange.bind(this);
+    this.toggleSplit = this.toggleSplit.bind(this);
   }
+
+  toggleSplit() {
+    this.setState({
+      splitButtonOpen: !this.state.splitButtonOpen
+    });
+  }
+
+
 
   onClickAddValues() {
-      this.props.addItems(this.sanitizeInput(this.state.textareaVal));
+    this.props.addItems(this.sanitizeInput(this.state.textareaVal));
   }
 
-  onCurrentCollectionChange(event) {
-    this.setState({currentCollection: event.target.value});
-    this.props.selectCollection(event.target.value);
+  getCollectionName(id) {
+    return this.props.collections.find(collection => collection.id === id).name;
+  }
+
+  onCurrentCollectionChange(id) {
+    this.setState({ currentCollection: this.getCollectionName(id) });
+    this.props.selectCollection(id);
   }
 
   sanitizeInput(unsanitizedString) {
     return unsanitizedString
       .trim()
-      .split(',')
-      .map(item => item
-        .trim()
-        .split(' '))
+      .split(",")
+      .map(item => item.trim().split(" "))
       .flat()
       .filter(a => a)
-      .map(item => item.replace('#', ''));
+      .map(item => item.replace("#", ""));
   }
 
   render() {
     return (
       <Row>
-        <Input
-          type="textarea"
-          placeholder="item1, item2, item3 or item1 item2 item3"
-          value={this.state.textareaVal}
-          onChange={event => this.setState({ textareaVal: event.target.value })}
-        />
-        <Button onClick={this.onClickAddValues}>Add values</Button>
-
-        <Input type="select" value={this.state.currentCollection} onChange={this.onCurrentCollectionChange}>
-            {this.props.collections.map(collection => {
-                return <option value={collection.id} key={'selectcol-' + collection.id}>{collection.name}</option>
-            })}
-        </Input>
+        <InputGroup>
+          <InputGroupButtonDropdown
+            addonType="prepend"
+            isOpen={this.state.splitButtonOpen}
+            toggle={this.toggleSplit}
+          >
+            <Button onClick={this.onClickAddValues}>
+              Add to <strong>{this.state.currentCollection}</strong>
+            </Button>
+            <DropdownToggle split />
+            <DropdownMenu>
+              {this.props.collections.map(collection => {
+                return (
+                  <DropdownItem
+                    onClick={() => {
+                      this.onCurrentCollectionChange(collection.id);
+                    }}
+                    key={"selectcol-" + collection.id}
+                  >
+                    {collection.name}
+                  </DropdownItem>
+                );
+              })}
+            </DropdownMenu>
+          </InputGroupButtonDropdown>
+          <Input
+            type="textarea"
+            placeholder="item1, item2, item3 or item1 item2 item3"
+            value={this.state.textareaVal}
+            onChange={event =>
+              this.setState({ textareaVal: event.target.value })
+            }
+          />
+        </InputGroup>
       </Row>
     );
   }
